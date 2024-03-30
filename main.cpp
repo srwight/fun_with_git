6b3ef6e69ec8b914449e019831ef8dbf035b9c23
Stephen Wight <srwight@gmail.com>
2024-03-30 13:22:38 -0500

    Add first arg to output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, ";
+    if(argc > 1){
+        std::cout << argv[1] << std::endl;
+    }
+    else{
+        std::cout << "world!" << std::endl;
+    }
     return 0;
 }
